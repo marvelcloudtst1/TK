d447689174471e131cc85a34f6d0b2a99d7c6ec7
marvelcloudtst1 <122812509+marvelcloudtst1@users.noreply.github.com>
2023-01-17 13:48:14 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -35,98 +35,82 @@ def add_handlers(bot: TelegramClient):
     
     bot.add_event_handler(
         handle_leech_command,
-        events.NewMessage(pattern=command_process(get_command("LEECH")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("LEECH")))
     )
     
     bot.add_event_handler(
         handle_purge_command,
-        events.NewMessage(pattern=command_process(get_command("PURGE")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("PURGE")))
     )
     
     bot.add_event_handler(
         handle_pauseall_command,
-        events.NewMessage(pattern=command_process(get_command("PAUSEALL")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("PAUSEALL")))
     )
     
     bot.add_event_handler(
         handle_resumeall_command,
-        events.NewMessage(pattern=command_process(get_command("RESUMEALL")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("RESUMEALL")))
     )
 
     bot.add_event_handler(
         handle_status_command,
-        events.NewMessage(pattern=command_process(get_command("STATUS")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("STATUS")))
     )
 
     bot.add_event_handler(
         handle_u_status_command,
-        events.NewMessage(pattern=command_process(get_command("USTATUS")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("USTATUS")))
     )
 
     bot.add_event_handler(
         handle_settings_command,
-        events.NewMessage(pattern=command_process(get_command("SETTINGS")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("SETTINGS")))
     )
 
     bot.add_event_handler(
         handle_exec_message_f,
-        events.NewMessage(pattern=command_process(get_command("EXEC")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("EXEC")))
     )
     
     bot.add_event_handler(
         upload_document_f,
-        events.NewMessage(pattern=command_process(get_command("UPLOAD")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("UPLOAD")))
     )
 
     bot.add_event_handler(
         handle_ytdl_command,
-        events.NewMessage(pattern=command_process(get_command("YTDL")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("YTDL")))
     )
 
     bot.add_event_handler(
         handle_ytdl_playlist,
-        events.NewMessage(pattern=command_process(get_command("PYTDL")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("PYTDL")))
     )
     
     bot.add_event_handler(
         about_me,
-        events.NewMessage(pattern=command_process(get_command("ABOUT")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("ABOUT")))
     )
 
     bot.add_event_handler(
         get_logs_f,
-        events.NewMessage(pattern=command_process(get_command("GETLOGS")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("GETLOGS")))
     )
     
     bot.add_event_handler(
         handle_test_command,
-        events.NewMessage(pattern="/test",
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern="/test")
     )
 
     bot.add_event_handler(
         handle_server_command,
-        events.NewMessage(pattern=command_process(get_command("SERVER")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("SERVER")))
     )
     
     bot.add_event_handler(
         set_password_zip,
-        events.NewMessage(pattern=command_process("/setpass"),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process("/setpass"))
     )
 
     bot.add_event_handler(
@@ -141,26 +125,22 @@ def add_handlers(bot: TelegramClient):
 
     bot.add_event_handler(
         clear_thumb_cmd,
-        events.NewMessage(pattern=command_process(get_command("CLRTHUMB")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("CLRTHUMB")))
     )
 
     bot.add_event_handler(
         set_thumb_cmd,
-        events.NewMessage(pattern=command_process(get_command("SETTHUMB")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("SETTHUMB")))
     )
     
     bot.add_event_handler(
         speed_handler,
-        events.NewMessage(pattern=command_process(get_command("SPEEDTEST")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("SPEEDTEST")))
     )
 
     bot.add_event_handler(
         cleardata_handler,
-        events.NewMessage(pattern=command_process(get_command("CRLDATA")),
-        chats=get_val("ALD_USR"))
+        events.NewMessage(pattern=command_process(get_command("CRLDATA")))
     )
 
     #signal.signal(signal.SIGINT, partial(term_handler,client=bot))
